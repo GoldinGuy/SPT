12a531fc75bc495ae0a7fb8c467cf2eb98999f25
Goldin Guy <sethgoldin27@gmail.com>
2019-07-20 13:44:36 -0400

    map 2

--- expo/screens/HomeScreen.js
+++ expo/screens/HomeScreen.js
@@ -63,12 +63,6 @@ export default class HomeScreen extends React.Component {
 
 
 
-
-
-
-
-
-
 // export default class HomeScreen extends React.Component {
 //   constructor(props){
 //     super(props);
@@ -182,4 +176,77 @@ export default class HomeScreen extends React.Component {
 //             </View>
 //         );
 //     }
+// }
+//
+// -----------------
+//
+// import React from 'react';
+// import { Dimensions, Platform, StyleSheet, Text, View } from 'react-native';
+// import { Location, Permissions } from 'expo';
+// import MapView, { AnimatedRegion } from 'react-native-maps';
+//
+// export default class HomeScreen extends React.Component {
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       region: null,
+//       initialRegion: null,
+//     }
+//   }
+//
+//   async getCurrentLocation() {
+//     navigator.geolocation.getCurrentPosition(
+//         position => {
+//         let region = {
+//                 latitude: parseFloat(position.coords.latitude),
+//                 longitude: parseFloat(position.coords.longitude),
+//                 latitudeDelta: 5,
+//                 longitudeDelta: 5
+//             };
+//             this.setState({
+//                 initialRegion: region
+//             });
+//         },
+//         error => console.log(error),
+//         {
+//             enableHighAccuracy: true,
+//             timeout: 20000,
+//             maximumAge: 1000
+//         }
+//     );
+// }
+//
+//   componentDidMount(){
+//     this.getCurrentLocation();
+//   }
+//
+//   goToInitialLocation() {
+//     let initialRegion = Object.assign({}, this.state.initialRegion);
+//     initialRegion["latitudeDelta"] = 0.005;
+//     initialRegion["longitudeDelta"] = 0.005;
+//     this.mapView.animateToRegion(initialRegion, 2000);
+//   }
+//
+//   render() {
+//     return (
+//       <MapView
+//         provider="google"
+//         style={{
+//           position: 'absolute',
+//           top: 50,
+//           bottom: 0,
+//           left: 0,
+//           right: 0,
+//           backgroundColor: 'white'
+//         }}
+//         region={this.state.mapRegion}
+//         followUserLocation={true}
+//         ref={ref => (this.mapView = ref)}
+//         zoomEnabled={true}
+//         showsUserLocation={true}
+//         onMapReady={this.goToInitialRegion.bind(this)}
+//         initialRegion={this.state.initialRegion}>
+//       </MapView>
+//     )
+//   }
 // }
